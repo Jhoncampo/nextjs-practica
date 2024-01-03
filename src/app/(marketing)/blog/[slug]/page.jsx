9ecd411ebd8b0dd57f55f1919e15794f8c35124c
eduard <eduardcamayo13@gmail.com>
2024-01-03 10:04:00 -0500

    Next js pt3

--- src/app/(marketing)/blog/[slug]/page.jsx
+++ src/app/(marketing)/blog/[slug]/page.jsx
@@ -1,12 +1,27 @@
 import TitlePage from '@/components/pageComponents/TitlePage'
 import React from 'react'
+import { POST } from '../page'
+import Link from 'next/link'
+
+export async function generateMetadata ({params}) {
+  const post = POST.find((post) => post.slug === params.slug)
+  return {
+    title: post.title,
+    description:  post.title + " - description"
+  }
+}
+
+
 
 const Slug = ({params}) => {
-    console.log(params)
+  const post = POST.find((post) => post.slug === params.slug)
   return (
     <>
-    <TitlePage title="Slug"/>
-    <p className='text-center'>{params.slug}</p>
+    <TitlePage title={post.title}/>
+    <p className='text-center mb-5'>{post.content}</p>
+    <div className="text-center">
+      <Link className='bg-indigo-600 px-6 py-2 text-white rounded hover:text-lg ease-linear' href="/blog" >Volver</Link>
+    </div>
     </>
   )
 }
--- src/app/(marketing)/page.jsx
+++ src/app/(marketing)/page.jsx
@@ -1,9 +1,11 @@
 import TitlePage from "@/components/pageComponents/TitlePage";
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
   return <>
     <TitlePage title="Home"/>
+    <Link href="/blog">Blog</Link>
   </>
 };
 
